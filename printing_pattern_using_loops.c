af476e05c0ea7a83f49f0610b9b945909f2c0a34
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-08-26 23:19:47 +0530

    Create printing_pattern_using_loops.c

---
+++ printing_pattern_using_loops.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+    
+    int size = 2 * n - 1; 
+    
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int min_dist = i < j ? i : j;
+            min_dist = min_dist < size - i - 1 ? min_dist : size - i - 1;
+            min_dist = min_dist < size - j - 1 ? min_dist : size - j - 1;
+            printf("%d ", n - min_dist);
+        }
+        printf("\n");  
+    }
+
+    return 0;
+}
