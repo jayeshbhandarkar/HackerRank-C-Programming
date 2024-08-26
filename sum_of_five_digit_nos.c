1d5cc2a9d112ea13a611844c950da3f80b03a691
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-08-26 23:39:04 +0530

    Update sum_of_five_digit_nos.c

--- sum_of_five_digit_nos.c
+++ sum_of_five_digit_nos.c
@@ -3,20 +3,23 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() {
+int main() 
+{
 	
     int n;
     scanf("%d", &n);
     //Complete the code to calculate the sum of the five digits on n.
     int last_num,temp,sum=0;
     scanf("%d", &n);
-        temp = n;
-            while(temp > 0)
-                {
-                        last_num = temp %10;
-                                sum = sum + last_num;
-                                        temp = temp/10;
-                                            }
-                                                printf("%d",sum);
-    return 0;
+    temp = n;
+           
+    while(temp > 0)
+    {
+	last_num = temp %10;
+        sum = sum + last_num;
+	temp = temp/10;
+     }
+	
+     printf("%d",sum);
+     return 0;
 }
