4a1c1c69a0c0ee80b639daf3d336bd19587aab50
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-08-26 23:33:28 +0530

    Update digit_frequency.c

--- digit_frequency.c
+++ digit_frequency.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main() 
+{
     char num[1000];
     scanf("%s", num);
     int freq[10] = {0};
     
-    for (int i = 0; i < strlen(num); i++) {
-        if (num[i] >= '0' && num[i] <= '9') {
+    for (int i = 0; i < strlen(num); i++) 
+    {
+        if (num[i] >= '0' && num[i] <= '9') 
+        {
             freq[num[i] - '0']++;
         }
     }
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) 
+    {
         printf("%d ", freq[i]);
     }
     
