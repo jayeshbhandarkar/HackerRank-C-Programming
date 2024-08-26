8b140277fe817d32efb65d414f8920c0188da19e
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-08-26 23:10:45 +0530

    Create sum_and_difference_of_two_nos.c

---
+++ sum_and_difference_of_two_nos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int var1, var2;
+    	float var3, var4;
+        
+        	scanf("%d %d", &var1, &var2);
+            	scanf("%f %f", &var3, &var4);
+                
+                	printf("%d %d", (var1+var2), (var1-var2));
+                    	printf("\n");
+                        	printf("%.1f %.1f", (var3+var4), (var3-var4));
+    
+    return 0;
+}
