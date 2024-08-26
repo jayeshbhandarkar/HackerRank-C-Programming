7253da5a4361cfe08cce8cfe26c33074c942b528
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-08-26 23:37:13 +0530

    Update printing_tokens.c

--- printing_tokens.c
+++ printing_tokens.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main() 
+{
     char sentence[1000];
     fgets(sentence, sizeof(sentence), stdin);
     char *token = strtok(sentence, " \n");
     
-    while (token != NULL) {
+    while (token != NULL) 
+    {
         printf("%s\n", token);
         token = strtok(NULL, " \n");
     }
