076557839e59ad10a7623f76c6aea2bd8c8f5afc
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-08-26 23:29:00 +0530

    Update 1D_Arrays.c

--- 1D_Arrays.c
+++ 1D_Arrays.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main() 
+{
     int n;
     scanf("%d", &n);
     int *arr = (int*)malloc(n * sizeof(int));
     
-    if (arr == NULL) {
+    if (arr == NULL) 
+    {
         printf("Memory allocation failed");
         return 1;
     }
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         scanf("%d", &arr[i]);
     }
 
     int sum = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         sum += arr[i];
     }
 
